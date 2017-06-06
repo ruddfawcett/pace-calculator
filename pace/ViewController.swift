d6c1fa76edd8713273d727b951c859ab176b0964
Rudd Fawcett <rudd.fawcett@gmail.com>
2017-06-06 18:17:36 -0400

    Initial Commit

---
+++ pace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pace
+//
+//  Created by Rudd Fawcett on 6/6/17.
+//  Copyright © 2017 Rudd Fawcett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
